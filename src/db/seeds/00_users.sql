8e056c507d2ce0a348e7d58518c5694f99234943
Jay Zhou <hollowic@hotmail.com>
2020-02-16 11:21:42 -0800

    added a few more

--- src/db/seeds/00_users.sql
+++ src/db/seeds/00_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users
     (username, password, profile_pic, created_at)
 VALUES
-    ('Alice', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://img.icons8.com/ios-filled/344/user-female-circle.png', '2020-01-10T22:09:47.778Z'),
     ('Jay', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://img.icons8.com/ios-filled/344/user-male-circle.png', '2020-01-11T22:01:32.778Z'),
+    ('Alice', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://img.icons8.com/ios-filled/344/user-female-circle.png', '2020-01-10T22:09:47.778Z'),
     ('Edison', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://img.icons8.com/ios-filled/344/user-male-circle.png', '2020-01-11T22:01:32.778Z');
--- src/db/seeds/02_images.sql
+++ src/db/seeds/02_images.sql
@@ -1,5 +1,7 @@
 INSERT INTO images
     (owner_id, longitude, latitude, description, url, views)
 VALUES
-    (1, 50.036278, -123.119603, 'Brandywine Falls Provincial Park', 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-15/e35/50477914_374092603389221_95756748038210597_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=fY0UfhSRY5MAX95kU2S&oh=4999f70f86873f168a21a3d7636fbc8c&oe=5EE1CB93', 4);
+    (1, 50.036278, -123.119603, 'Brandywine Falls Provincial Park', 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-15/e35/50477914_374092603389221_95756748038210597_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=fY0UfhSRY5MAX95kU2S&oh=4999f70f86873f168a21a3d7636fbc8c&oe=5EE1CB93', 3),
+    (1, 49.373019, -123.292891, 'Whytecliff Park', 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-15/e35/51953649_607800683004431_7593307819852670582_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=MyKnH_sCwMYAX9xkZgd&oh=909164b581466c3dfb1fd0d92689624c&oe=5ECE8439', 3),
+    (1, 50.1350757, -123.1304821, 'Brandywine Falls Provincial Park', 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-15/e35/51034562_688077208255546_2485706674706050571_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=qvpdh6L40ZYAX_uLPc4&oh=641e9e61844b1e5b26524c84153c0dd1&oe=5EDFD2C3', 3);
  
