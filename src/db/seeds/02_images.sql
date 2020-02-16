2bc58e78408346f2ce25063881838e347472b0db
Jay Zhou <hollowic@hotmail.com>
2020-02-15 17:22:48 -0800

    added schema table and some seeds

---
+++ src/db/seeds/02_images.sql
@@ -0,0 +1,5 @@
+INSERT INTO images
+    (owner_id, longitude, latitude, description, url, views)
+VALUES
+    (1, 50.036278, -123.119603, 'Brandywine Falls Provincial Park', 'https://scontent-sea1-1.cdninstagram.com/v/t51.2885-15/e35/50477914_374092603389221_95756748038210597_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=fY0UfhSRY5MAX95kU2S&oh=4999f70f86873f168a21a3d7636fbc8c&oe=5EE1CB93', 4);
+ 
