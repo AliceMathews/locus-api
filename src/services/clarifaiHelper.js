bd831fabb2422bd3782b7a043964ef5cf311ca6a
AliceMathews <alice_mathews@hotmail.co.uk>
2020-02-19 14:21:32 -0800

    added filter function to remove no person tag and limit to 12

--- src/services/clarifaiHelper.js
+++ src/services/clarifaiHelper.js
@@ -1,14 +1,23 @@
-const clarifai = require('./clarifai');
+const clarifai = require("./clarifai");
 
-const getImageTags = (url) => {
-  return clarifai.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+const getImageTags = url => {
+  return clarifai.models
+    .initModel({
+      id: Clarifai.GENERAL_MODEL,
+      version: "aa7f35c01e0642fda5cf400f543e7c40"
+    })
     .then(generalModel => {
       return generalModel.predict(url);
     })
     .then(response => {
-      var concepts = response['outputs'][0]['data']['concepts']
+      var concepts = response["outputs"][0]["data"]["concepts"];
       return Promise.resolve(concepts);
     });
 };
 
 exports.getImageTags = getImageTags;
+
+const filterImageTags = tags => {
+  return tags.filter(tag => tag.name !== "no person").slice(0, 12);
+};
+exports.filterImageTags = filterImageTags;
